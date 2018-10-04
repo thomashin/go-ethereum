9d7925a31a7e3af1bacf77d9889025e0b03cccad
thomashin <dn2757@gmail.com>
2018-10-04 13:39:51 +0900

    console: add pls variable

--- console/console.go
+++ console/console.go
@@ -137,7 +137,7 @@ func (c *Console) init(preload []string) error {
 	if err != nil {
 		return fmt.Errorf("api modules: %v", err)
 	}
-	flatten := "var eth = web3.eth; var personal = web3.personal; "
+	flatten := "var pls = web3.pls; var eth = web3.eth; var personal = web3.personal; "
 	for api := range apis {
 		if api == "web3" {
 			continue // manually mapped or ignore
